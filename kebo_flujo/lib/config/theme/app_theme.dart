c9a5f0f985b568792131684ed0b44d0d3134d813
davidethc <davidecondet@gmail.com>
2024-09-07 14:45:59 -0500

    Clase Gradiente Login

--- kebo_flujo/lib/config/theme/app_theme.dart
+++ kebo_flujo/lib/config/theme/app_theme.dart
@@ -8,11 +8,25 @@ const List<Color> colorThemes = [
   Colors.black,
   Colors.green,
   Colors.cyanAccent,
+
   // ... otros colores si los hay ...
 ];
 
 class AppTheme {
   ThemeData theme() {
     return ThemeData(useMaterial3: true, colorSchemeSeed: colorThemes[2]);
+  }
+}
+
+class LoginGradient {
+  static const Color startColor = Color(0xFFE6D8FF);
+  static const Color endColor = Color(0xFFB388FF);
+
+  static LinearGradient get gradient {
+    return LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [startColor, endColor],
+    );
   }
 }
--- kebo_flujo/lib/screens/login.dart
+++ kebo_flujo/lib/screens/login.dart
@@ -68,147 +68,152 @@ class _LoginPageState extends State<LoginPage> {
     }
 
     return Scaffold(
-      backgroundColor: Color(0xFFE6D8FF),
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(children: [
-              // Logo
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
-                child: Image.asset(
-                  'lib/images/ic_logo.png', // Asegúrate de que esta ruta sea correcta
-                  width: 100, // Ajusta el tamaño según necesites
-                  height: 100,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LoginGradient.gradient,
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(children: [
+                // Logo
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+                  child: Image.asset(
+                    'lib/images/ic_logo.png', // Asegúrate de que esta ruta sea correcta
+                    width: 100, // Ajusta el tamaño según necesites
+                    height: 100,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
+                const SizedBox(
+                  height: 50,
+                ),
 
-              //welcome back,you've been missided
-              Text(
-                'welcome back you/ve been missided',
-                style: TextStyle(
-                    color: colorThemes[1],
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              //username text field
-              MyTextField(
-                controller: userNameController,
-                hinText: 'email',
-                obscureText: false,
-              ),
+                //welcome back,you've been missided
+                Text(
+                  'welcome back you/ve been missided',
+                  style: TextStyle(
+                      color: colorThemes[1],
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                //username text field
+                MyTextField(
+                  controller: userNameController,
+                  hinText: 'email',
+                  obscureText: false,
+                ),
 
-              const SizedBox(
-                height: 10,
-              ),
+                const SizedBox(
+                  height: 10,
+                ),
 
-              //password text field
-              MyTextField(
-                controller: passwordController,
-                hinText: 'password',
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              //forgot password
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                //password text field
+                MyTextField(
+                  controller: passwordController,
+                  hinText: 'password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                //forgot password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password',
+                        style:
+                            TextStyle(color: colorThemes[3].withOpacity(0.9)),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                //sing in button
+                MyButton(
+                  text: 'Sign In',
+                  onTap: signUserIn,
+                ),
+
+                const SizedBox(
+                  height: 50,
+                ),
+
+                // or contunue with
+                Row(
                   children: [
-                    Text(
-                      'Forgot Password',
-                      style: TextStyle(color: colorThemes[3].withOpacity(0.9)),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: colorThemes[1],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(7),
+                      child: Text(
+                        'or continue with',
+                        style: TextStyle(color: colorThemes[3]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: colorThemes[1],
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              //sing in button
-              MyButton(
-                text: 'Sign In',
-                onTap: signUserIn,
-              ),
 
-              const SizedBox(
-                height: 50,
-              ),
+                const SizedBox(height: 30),
+                //google + apple sign in buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareTile(
+                      imagePath: 'lib/images/google.png',
+                      onTap: () => AuthService().signInWithGoogle(),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
 
-              // or contunue with
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: colorThemes[1],
+                /////
+
+                //////
+                ///
+
+                //not a member register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Not a member?'),
+                    const SizedBox(
+                      width: 4,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(7),
-                    child: Text(
-                      'or continue with',
-                      style: TextStyle(color: colorThemes[3]),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: colorThemes[1],
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 30),
-              //google + apple sign in buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareTile(
-                    imagePath: 'lib/images/google.png',
-                    onTap: () => AuthService().signInWithGoogle(),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-
-              /////
-
-              //////
-              ///
-
-              //not a member register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Not a member?'),
-                  const SizedBox(
-                    width: 4,
-                  ),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Text(
-                      'Register now ',
-                      style: TextStyle(
-                          color: colorThemes[5], fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
-              )
-              // ... resto de los widgets ...
-            ]),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text(
+                        'Register now ',
+                        style: TextStyle(
+                            color: colorThemes[5], fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                )
+                // ... resto de los widgets ...
+              ]),
+            ),
           ),
         ),
       ),
